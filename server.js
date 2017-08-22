c4d4fd983c220b39683ffcd4e970c418ab4ba5a6
TechBison <shreeshg4@gmail.com>
2017-08-22 08:50:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-aap.get('/article-one',function(req,res){
+app.get('/article-one',function(req,res){
     res.send('article-one is requested and will be served here');
 });
-aap.get('/article-two',function(req,res){
+app.get('/article-two',function(req,res){
     res.send('article-two is requested and will be served here');
 });
-aap.get('/article-three',function(req,res){
+app.get('/article-three',function(req,res){
     res.send('article-three is requested and will be served here');
 });
-aap.get('/article-four',function(req,res){
+app.get('/article-four',function(req,res){
     res.send('article-four is requested and will be served here');
 });
 
